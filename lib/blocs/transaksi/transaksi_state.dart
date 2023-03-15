61b1766e2bc1f85cda81299247b94879be68fe10
Kenny Jinhiro <jxnhiro@gmail.com>
2023-03-15 16:43:39 +0700

    Transaksi State

--- lib/blocs/transaksi/transaksi_state.dart
+++ lib/blocs/transaksi/transaksi_state.dart
@@ -2,9 +2,13 @@ part of 'transaksi_bloc.dart';
 
 abstract class TransaksiState extends Equatable {
   const TransaksiState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class TransaksiInitial extends TransaksiState {}
+
+class TransaksiLoading extends TransaksiState {}
+
+class TransaksiGetSuccess extends TransaksiState {}
